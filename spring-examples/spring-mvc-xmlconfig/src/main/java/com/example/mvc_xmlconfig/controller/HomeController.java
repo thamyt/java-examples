df5907c9f376172c4eeae97b53a1216a0f79310e
thamyt <thamyt75@yahoo.com.sg>
2021-02-20 00:11:36 +0800

    update spring security features

--- spring-examples/spring-mvc-xmlconfig/src/main/java/com/example/mvc_xmlconfig/controller/HomeController.java
+++ spring-examples/spring-mvc-xmlconfig/src/main/java/com/example/mvc_xmlconfig/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,18 @@ public class HomeController extends BaseController {
 	@Autowired
 	private HelloBean helloBean;
 	
+	@Autowired
+	BCryptPasswordEncoder encoder;
+	
 	@Resource(name="countryList")
 	Map<String, String> ctrList;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String home(Model model) {
 		ctrList.forEach((k, v) -> System.out.println((k + ":" + v)));
+		
+		System.out.println(encoder.encode("P@ssw0rd123"));
+		System.out.println(encoder.encode("P@ssw0rd123"));
 		
 		model.addAttribute("helloMsg", helloBean);
 		return "index.html";
